5a29314fb50bd5d4c77dd16b20198e04e6e52914
kimheilig1 <kh143@hdm-stuttgart.de>
2026-06-12 17:03:04 +0200

    Code geändert DB Todos

--- WB_KSD/database/migrations/1776233506302_create_todos_table.ts
+++ WB_KSD/database/migrations/1776233506302_create_todos_table.ts
@@ -4,19 +4,14 @@ export default class extends BaseSchema {
   protected tableName = 'todos'
 
   async up() {
-    this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
-      table.string('title').notNullable()
-      table.string('category').nullable()
-      table.string('priority').nullable()
-      table.boolean('is_completed').defaultTo(false)
-      table.string('file_path').nullable()
-      table.date('due_date').nullable() 
-      table.timestamps(true, true)
+    this.schema.alterTable(this.tableName, (table) => {
+      table.string('quadrant').nullable()
     })
   }
 
   async down() {
-    this.schema.dropTable(this.tableName)
+    this.schema.alterTable(this.tableName, (table) => {
+      table.dropColumn('quadrant')
+    })
   }
 }
